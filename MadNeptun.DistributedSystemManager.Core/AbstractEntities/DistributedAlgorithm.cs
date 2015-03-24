42025eea0f0407abba9b662e23cc15893f63e3bf
Mad Neptun <piotr.rachwal27@gmail.com>
2015-03-24 11:50:01 +0100

    rewrite to generic, refactor, move to another folder

---
+++ MadNeptun.DistributedSystemManager.Core/AbstractEntities/DistributedAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MadNeptun.DistributedSystemManager.Core.Objects;
+
+namespace MadNeptun.DistributedSystemManager.Core.AbstractEntities
+{
+    public abstract class DistributedAlgorithm<TIdType, TValue>
+    {
+        public abstract OperationResult<TIdType, TValue> Init(Message<TValue> message, IEnumerable<NodeId<TIdType>> neighbors);
+
+        public abstract OperationResult<TIdType, TValue> RecieveMessage(Message<TValue> message, NodeId<TIdType> sender, IEnumerable<NodeId<TIdType>> neighbors);
+    }
+}
---
+++ MadNeptun.DistributedSystemManager.Core/AbstractEntities/NetworkComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MadNeptun.DistributedSystemManager.Core.Objects;
+
+namespace MadNeptun.DistributedSystemManager.Core.AbstractEntities
+{
+    public abstract class NetworkComponent<TIdType, TValue>
+    {
+        public abstract void Send(Message<TValue> message, List<NodeId<TIdType>> recievers, NodeId<TIdType> sender);
+
+        public delegate void MessageRecieved(object sender, MessageRecievedEventArgs<TIdType, TValue> e);
+
+        public event MessageRecieved OnMessageRecieved;
+
+        /// <summary>
+        /// Warning! After recieving and formatting data function must call InformNode method in order to relay this message to node.
+        /// </summary>
+        public abstract void Recieve(Message<TValue> message, NodeId<TIdType> sender);
+
+        protected void InformNode(MessageRecievedEventArgs<TIdType, TValue> data)
+        {
+            if(OnMessageRecieved!=null)
+                OnMessageRecieved(this, data); 
+        }
+    }
+}
