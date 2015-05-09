2de763c1dc61b18342fbd1cffb5e02d20a3a0413
Mad Neptun <piotr.rachwal27@gmail.com>
2015-05-09 15:22:33 +0200

    service project add

---
+++ MadNeptun.DistributedSystemManager.Service/NetworkService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace MadNeptun.DistributedSystemManager.Service
+{
+    public partial class NetworkService : ServiceBase
+    {
+        public NetworkService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ MadNeptun.DistributedSystemManager.Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace MadNeptun.DistributedSystemManager.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new NetworkService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
